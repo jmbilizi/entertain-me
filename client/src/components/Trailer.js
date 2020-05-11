39d71594ab3b85a18e2b372e2073a146a6423a8b
grovem74 <matthewg74@gmail.com>
2020-05-10 19:02:14 -0600

    cleaned up the code to clear some warnings in the console

--- client/src/components/Trailer.js
+++ client/src/components/Trailer.js
@@ -8,11 +8,8 @@ const Trailer = (props) => {
             <div className='center-align'>
                 <h3>TRAILERS</h3>
                 <div className='trailer'>
-                    <iframe width="560" height="315" src={props.movieTrailer} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen embed='true'></iframe>
+                    <iframe title='trailers' width="560" height="315" src={props.movieTrailer} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen embed='true'></iframe>
                 </div>
-
-
-                {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/BELQgBy2gic" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
             </div>
         </>
     )
