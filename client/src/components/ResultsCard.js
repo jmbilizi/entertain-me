92d7cfc842d7638ba794c5a59a412e006211bb5c
grovem74 <matthewg74@gmail.com>
2020-05-15 16:15:41 -0600

    all movie/tv related API calls are working

--- client/src/components/ResultsCard.js
+++ client/src/components/ResultsCard.js
@@ -28,7 +28,7 @@ const ResultsCard = (props) => {
           <img
               className="network-logo"
               src={props.logo}
-              alt={"tv network"}
+              alt={props.provider}
             />
             <img
               className="backdrop-image"
@@ -39,7 +39,6 @@ const ResultsCard = (props) => {
             <div className="details">
               <h4 className="results-card-title">
                 {props.title}
-                {props.title2}
               </h4>
               <div className="result-btns">
                 <span onClick={addFavorite}>
