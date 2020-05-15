c2315a5f1b5e1fed81e807aab8f3780e66fba10a
grovem74 <matthewg74@gmail.com>
2020-05-15 13:24:52 -0600

    made adjustment to code to allow both movie and tv show searches

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,47 +9,47 @@ export default {
       `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=en-US&query=${title}&page=1&include_adult=false`
     );
   },
-  movieSearch: function (userInput) {
+  // movieSearch: function (userInput) {
+  //   return axios.get(
+  //     `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${userInput}&page=1&include_adult=false`
+  //   );
+  // },
+  // API call for movie poster, trailers and runtime -- need to credit COMBINED
+  trailerSearch: function (id, mediaType) {
     return axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${userInput}&page=1&include_adult=false`
+      `https://api.themoviedb.org/3/${mediaType}/${id}?api_key=${apiKey}&language=en-US&append_to_response=videos`
     );
   },
-  // API call for movie poster, trailers and runtime -- need to credit
-  trailerSearch: function (id) {
-    return axios.get(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US&append_to_response=videos`
-    );
-  },
-  // API call for movie rating -- need to credit
-  movieRatingSearch: function (id) {
-    return axios.get(
-      `https://api.themoviedb.org/3/movie/${id}/release_dates?api_key=${apiKey}`
-    );
-  },
-  // API call for similar movies -- need to credit
-  relatedMoviesSearch: function (id) {
+  // API call for movie rating -- need to credit    COMBINED
+  // movieRatingSearch: function (id) {
+  //   return axios.get(
+  //     `https://api.themoviedb.org/3/movie/${id}/release_dates?api_key=${apiKey}`
+  //   );
+  // },
+  // API call for similar movies -- need to credit COMBINED
+  relatedMoviesSearch: function (id) { 
     return axios.get(
       `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}&language=en-US&page=1`
     );
   },
 
     // API call for tv ratings -- need to credit
-    tvRatingSearch: function (id) {
+    ratingSearch: function (id, mediaType) {
       return axios.get(
-        `https://api.themoviedb.org/3/tv/${id}/content_ratings?api_key=${apiKey}&language=en-US`
+        `https://api.themoviedb.org/3/${mediaType}/${id}/${mediaType === 'tv' ? 'content_ratings' : 'release_dates' }?api_key=${apiKey}&language=en-US`
       );
     },
 
   // API call for similar movies -- need to credit
-  relatedTVSearch: function (id) {
+  relatedTVSearch: function (id, mediaType) {
     return axios.get(
-      `https://api.themoviedb.org/3/tv/${id}/similar?api_key=${apiKey}&language=en-US&page=1`
+      `https://api.themoviedb.org/3/${mediaType}/${id}/similar?api_key=${apiKey}&language=en-US&page=1`
     );
   },
   // API call for TV poster, trailers, network and next episode -- need to credit
-  tvTrailerSearch: function (tvID) {
+  tvTrailerSearch: function (id, mediaType) {
     return axios.get(
-      `https://api.themoviedb.org/3/tv/${tvID}?api_key=${apiKey}&language=en-US&append_to_response=videos`
+      `https://api.themoviedb.org/3/${mediaType}/${id}?api_key=${apiKey}&language=en-US&append_to_response=videos`
     );
   },
   // API call for trending movies -- need to credit
@@ -64,6 +64,14 @@ export default {
       `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}`
     );
   },
+
+// API call for trending tv shows -- need to credit
+celebSearch: function (userInput) {
+  return axios.get(
+    `https://api.themoviedb.org/3/person/${userInput}?api_key=${apiKey}&language=en-US`
+  );
+},
+
   // API call for trending celebrities  -- need to cache every 30 min
   trendingCelebritySearch: function () {
     return axios.get(
