2b2b8b7f07d5c836bc57c1df07ae92d24fa39b90
Lod180 <ilanashaffer89@gmail.com>
2020-05-18 21:08:35 -0600

    added favorites route and axios call

--- routes/favorites.js
+++ routes/favorites.js
@@ -2,3 +2,10 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+router.put('/', function (req, res) {
+    console.log(req.body.userId);
+    // find user and update
+    res.json({});
+});
+
+module.exports = router;
