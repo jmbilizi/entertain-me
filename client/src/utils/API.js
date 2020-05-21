d63a14e5d7bfc9a2af20cb73510b726287d6631b
grovem74 <matthewg74@gmail.com>
2020-05-21 11:04:57 -0600

    removed api key string from API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const apiKey = process.env.REACT_APP_API_KEY;
-const apiKey = '32a9ec1e57a6839fcf26fa958bca7cf5';
+import keys from './keys'
+const apiKey = keys.tmdbApiKey
 
 export default {
   // API call for initial tv or movie details -- need to credit
@@ -15,22 +15,14 @@ export default {
       `https://api.themoviedb.org/3/${mediaType}/${id}?api_key=${apiKey}&language=en-US&append_to_response=videos`
     );
   },
-  // API call for similar movies -- need to credit COMBINED
-  // relatedMoviesSearch: function (id) {
-  //   return axios.get(
-  //     `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}&language=en-US&page=1`
-  //   );
-  // },
-
   // API call for tv ratings -- need to credit
   ratingSearch: function (id, mediaType) {
     return axios.get(
       `https://api.themoviedb.org/3/${mediaType}/${id}/${
-        mediaType === 'tv' ? 'content_ratings' : 'release_dates'
+      mediaType === 'tv' ? 'content_ratings' : 'release_dates'
       }?api_key=${apiKey}&language=en-US`
     );
   },
-
   // API call for similar movies -- need to credit
   relatedSearch: function (id, mediaType) {
     return axios.get(
