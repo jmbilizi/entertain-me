39c24b86034d108625ce2ebfdd0c4f4aa7040fc0
grovem74 <matthewg74@gmail.com>
2020-05-20 21:11:55 -0600

    fixed bug preventing home page from rendering after login and adjusted some formatting for the celebrities page.

--- client/src/components/TrendingCelebritiesDefault.js
+++ client/src/components/TrendingCelebritiesDefault.js
@@ -5,17 +5,18 @@ const DefaultCelebAppearances = () => {
   return (
     <>
       <Row>
+      <h6 className="trending-celeb-title">Trending Celebrities</h6>
         <Col>
           <img
             className="trending-celeb-images"
             src="https://static.tvtropes.org/pmwiki/pub/images/rick_and_morty_8.jpg"
-            title="pic1"
+            title="Rick Sanchez"
             alt="default trending celebrities"
           />
           <img
             className="trending-celeb-images"
             src={require('../../src/assets/images/Vin-Diesel.jpg')}
-            title="pic2"
+            title="Vin Diesel"
             alt="default trending celebrities"
           />
         </Col>
@@ -25,14 +26,14 @@ const DefaultCelebAppearances = () => {
           <img
             className="trending-celeb-images"
             src="https://www.toonpool.com/user/18089/files/taylor_swift_1274625.jpg"
-            title="pic2"
+            title="Taylor Swift"
             alt="default trending celebrities"
           />
 
           <img
             className="trending-celeb-images"
             src="https://vignette.wikia.nocookie.net/ricksanchez/images/e/e8/Morty.png/revision/latest/top-crop/width/360/height/450?cb=20160605181037"
-            title="pic2"
+            title="Morty Smith"
             alt="default trending celebrities"
           />
         </Col>
