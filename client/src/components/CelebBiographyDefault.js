7a9db3c991d15e5e2641afc5a520a91f43c433c5
grovem74 <matthewg74@gmail.com>
2020-05-20 21:11:55 -0600

    fixed bug preventing home page from rendering after login and adjusted some formatting for the celebrities page.

--- client/src/components/CelebBiographyDefault.js
+++ client/src/components/CelebBiographyDefault.js
@@ -3,8 +3,8 @@ import React from "react";
 const CelebBiographyDefault = () => {
   return (
     <>
+      <h6 className="biography-title">Biography</h6>
       <p className='celeb-biography'>A brief biography of the celebrity will be displayed here.
-
       </p>
     </>
   );
