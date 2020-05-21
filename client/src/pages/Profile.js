2da4709bb2483a69a080f0eebe5f2c5fa7ecaba2
Admin <admin@Admins-MacBook-Pro.local>
2020-05-21 16:36:45 -0600

    set a templete profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import 'materialize-css';
-import { Container } from 'react-materialize';
-import { Redirect } from 'react-router-dom';
-import { ContainerWrapper } from '../assets/styles';
+import React from "react";
+import "materialize-css";
+import { Container } from "react-materialize";
+import { Redirect } from "react-router-dom";
+import { ContainerWrapper } from "../assets/styles";
 
 const Profile = (props) => {
   const { token } = props;
-  if (!token) return <Redirect to={'/login'} />;
+  if (!token) return <Redirect to={"/login"} />;
 
   return (
     <ContainerWrapper>
-      <Container></Container>
+      <Container>
+        <div className="row">
+          <div className="col s12 center-align">
+            <h2 className="white-text">Profile</h2>
+            <br />
+            <h6 className="white-text">
+              First Name: XXXXXXXXX
+              {/* {user.name} */}
+            </h6>
+            <br />
+            <h6 className="white-text">
+              Last Name: XXXXXXXXX
+              {/* {user.name} */}
+            </h6>
+            <br />
+            <h6 className="white-text">
+              Email: YYYYYY@gmail.com
+              {/* {user.email} */}
+            </h6>
+            {/* <p>{`Joined ${new Date(user.created).toDateString()}`}</p> */}
+          </div>
+        </div>
+      </Container>
     </ContainerWrapper>
   );
 };
