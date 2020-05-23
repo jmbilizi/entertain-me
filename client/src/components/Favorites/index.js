a89a19791fc1a2b86d3fa897c2ec0d4058618073
Lod180 <ilanashaffer89@gmail.com>
2020-05-22 22:07:06 -0600

    added Favorites deleting functionality front and back end

--- client/src/components/Favorites/index.js
+++ client/src/components/Favorites/index.js
@@ -7,17 +7,25 @@ const Favorites = (props) => {
     console.log(e);
     props.mediaSearch(e)
   };
-const { favorites} = props
-console.log({favorites})
+
+  const deleteItem = (mediaName) => {
+    props.deleteMedia(mediaName);
+  }
+
+  const { favorites } = props
+  console.log({ favorites })
   return (
     <>
       <h6 className="center-align title">{props.heading}</h6>
       <Collection className="frame">
-        {favorites.map(item =>  <CollectionItem onClick={( )=>getDetails(item.media_name)}>
-          {item.media_name}<span className="material-icons remove">remove_circle</span>
-        </CollectionItem> )}
-       
-        
+        {favorites.map(item => 
+        (<CollectionItem>
+          <span onClick={() => getDetails(item.media_name)}>{item.media_name}</span>
+          <span onClick={ () => deleteItem(item.media_name) } className="material-icons remove">remove_circle</span>
+        </CollectionItem>)
+        )}
+
+
       </Collection>
     </>
 
