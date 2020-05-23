fe8c83036c2b24c13e8e4cfd6fe957d7a99a16b5
grovem74 <matthewg74@gmail.com>
2020-05-23 12:04:45 -0600

    removed slider from sign in page

--- client/src/components/loginSignup/LeftSideContent.js
+++ client/src/components/loginSignup/LeftSideContent.js
@@ -1,51 +1,49 @@
 import React from 'react';
-import { Slider, Slide, Caption } from 'react-materialize';
+import { MediaBox } from 'react-materialize';
 import './carousel.css';
 
 function LeftSideContent() {
   return (
     <>
-   <Slider
-  fullscreen={false}
-  options={{
-    duration: 1000,
-    height: 400,
-    indicators: false,
-    interval: 6000
-  }}
->
-  <Slide image={<img alt='' src={require('./images/movie.jpg')}/>}>
-    <Caption>
-      <h3>
-        This is our big Tagline!
-      </h3>
-      <h5 className='light grey-text text-lighten-3'>
-        Here's our small slogan.
-      </h5>
-    </Caption>
-  </Slide>
-  <Slide image={<img alt='' src={require('./images/show.jpg')}/>}>
-    <Caption placement='left'>
-      <h3>
-        Left Aligned Caption
-      </h3>
-      <h5 className='light grey-text text-lighten-3'>
-        Here's our small slogan.
-      </h5>
-    </Caption>
-  </Slide>
-  <Slide image={<img alt='' src={require('./images/celebrity.jpg')}/>}>
-    <Caption placement='right'>
-      <h3>
-        Right Aligned Caption
-      </h3>
-      <h5 className='light grey-text text-lighten-3'>
-        Here's our small slogan.
-      </h5>
-    </Caption>
-  </Slide>
-</Slider>
-
+      <MediaBox
+        id="MediaBox_1"
+        options={{
+          inDuration: 275,
+          onCloseEnd: null,
+          onCloseStart: null,
+          onOpenEnd: null,
+          onOpenStart: null,
+          outDuration: 200
+        }}
+      >
+        <img alt='popcorn' src={require('./images/movie.jpg')} width="375" />
+      </MediaBox>
+      <MediaBox
+        id="MediaBox_2"
+        options={{
+          inDuration: 275,
+          onCloseEnd: null,
+          onCloseStart: null,
+          onOpenEnd: null,
+          onOpenStart: null,
+          outDuration: 200
+        }}
+      >
+        <img alt='popcorn' src={require('./images/show.jpg')} width="375" />
+      </MediaBox>
+      <MediaBox
+        id="MediaBox_3"
+        options={{
+          inDuration: 275,
+          onCloseEnd: null,
+          onCloseStart: null,
+          onOpenEnd: null,
+          onOpenStart: null,
+          outDuration: 200
+        }}
+      >
+        <img alt='popcorn' src={require('./images/celebrity.jpg')} width="375" />
+      </MediaBox>
     </>
   );
 }
