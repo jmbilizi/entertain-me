9d2077a0adbde7addaff740bf2d7940c03d32c1e
Admin <admin@Admins-MacBook-Pro.local>
2020-05-27 22:53:02 -0600

    working on profile page(Still in progress)

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,38 +4,28 @@ import { Container } from "react-materialize";
 import { Redirect } from "react-router-dom";
 import { ProfilePageWrapper } from "../assets/styles";
 import { user } from "../utils/helpers";
+import Axios from "axios";
 
 const Profile = (props) => {
   const { token } = props;
 
-  const [state, setState] = useState({
-    userProfile: "",
-    redirectToLogin: false,
-  });
+  // const [state, setState] = useState({
+  //   userProfile: "",
+  //   redirectToLogin: false,
+  // });
 
-  useEffect(() => {
-    const userId = user()._id;
+  // const userId = user()._id;
 
-    fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-type": "application/json",
-        Authorization: `Beater ${token}`,
-      },
-    })
-      .then((response) => {
-        return response.json();
-        console.log(response);
-      })
-      .then((data) => {
-        if (data.error) {
-          this.setState({ redirectToLogin: true });
-        } else {
-          this.setState({ userProfile: data });
-        }
-      });
-  });
+  // useEffect(() => {
+  //   console.log(`testing user: ${userId}`);
+
+  //   Axios(`/user/${userId}`, {
+  //     method: "GET",
+  //   }).then((response) => {
+  //     console.log(response);
+  //     // return response.json();
+  //   });
+  // });
 
   if (!token) return <Redirect to={"/login"} />;
 
@@ -47,12 +37,50 @@ const Profile = (props) => {
   return (
     <ProfilePageWrapper>
       <div className="container white-text">
-        <h2 className="mt-5 mb-5">Profile</h2>
-        <p>First Name: {`${user().fname}`}</p>
-        <p>Last Name: {`${user().lname}`}</p>
-        <p>Email: {`${user().username}`}</p>
-        <p>{`Joined: ${new Date(user().iat).toDateString()}`}</p>
-        <ul></ul>
+        <div className="row">
+          <h3>Profile</h3>
+        </div>
+        <div className="row">
+          <div className="col s6">
+            <img
+              class="materialboxed"
+              data-caption="A picture of a way with a group of trees in a park"
+              width="250"
+              src="https://lorempixel.com/800/400/nature/4"
+            ></img>
+          </div>
+          <div className="col s6">
+            <p>First Name: {`  ${user().fname}`}</p>
+            <p>Last Name: {`   ${user().lname}`}</p>
+            <p>Email: {`       ${user().username}`}</p>
+            <p>{`Joined:       ${new Date(user().created).toDateString()}`}</p>
+            <hr />
+            <div className="row">
+              <div className="col s6">
+                <a class="btn teal">Update</a>
+              </div>
+              <div className="col s6">
+                <button class="btn red">Delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <br />
+        <div className="row center-align">
+          <h6>Calendar</h6>
+        </div>
+        <div className="row center-align">
+          <div className="col s4">
+            <h6>Favorites movies</h6>
+          </div>
+          <div className="col s4">
+            <h6>Favorites TV shows</h6>
+          </div>
+          <div className="col s4">
+            <h6>Favorites Celebrities</h6>
+          </div>
+        </div>
       </div>
     </ProfilePageWrapper>
   );
