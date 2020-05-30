d4e749461ab81a330e526b847c4f952b6f0640cb
Admin <admin@Admins-MacBook-Pro.local>
2020-05-30 14:29:41 -0600

    commented up the chat

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import "materialize-css";
 import { Container, Row, Col } from "react-materialize";
 import Headlines from "../../components/Headlines";
-import Chat from '../../components/Chat/Chat'
-import "./style.css"
+import Chat from "../../components/Chat/Chat";
+import "./style.css";
 import API from "../../utils/API.js";
-import $ from 'jquery';
-
-
+import $ from "jquery";
 
 // const users = [
 //   {
@@ -58,53 +56,72 @@ import $ from 'jquery';
 //     });
 //   }
 
-
 // }
 
-
 const Home = (props) => {
-
   const { token } = props;
 
-
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text1'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>MOVIE</span>.</h5>`);
-  }, 2800)
+    $("#home").html(
+      `<br><br><h5 id='home-text1'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>MOVIE</span>.</h5>`
+    );
+  }, 2800);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text2'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>SHOW</span>.</h5>`);
-  }, 4000)
+    $("#home").html(
+      `<br><br><h5 id='home-text2'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>SHOW</span>.</h5>`
+    );
+  }, 4000);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>BOOK</span>.</h5>`);
-  }, 4250)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>BOOK</span>.</h5>`
+    );
+  }, 4250);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>GAME</span>.</h5>`);
-  }, 4500)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>GAME</span>.</h5>`
+    );
+  }, 4500);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>CHARACTER</span>.</h5>`);
-  }, 4750)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>CHARACTER</span>.</h5>`
+    );
+  }, 4750);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>EPISODE</span>.</h5>`);
-  }, 5000)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>EPISODE</span>.</h5>`
+    );
+  }, 5000);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>SONG</span>.</h5>`);
-  }, 5250)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>SONG</span>.</h5>`
+    );
+  }, 5250);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text4'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>SCENE</span>.</h5>`);
-  }, 5500)
+    $("#home").html(
+      `<br><br><h5 id='home-text4'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>SCENE</span>.</h5>`
+    );
+  }, 5500);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>PERSON</span>.</h5>`);
-  }, 5750)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>PERSON</span>.</h5>`
+    );
+  }, 5750);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>ALBUM</span>.</h5>`);
-  }, 6000)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>ALBUM</span>.</h5>`
+    );
+  }, 6000);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>THING</span>.</h5>`);
-  }, 6250)
+    $("#home").html(
+      `<br><br><h5 id='home-text3'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>THING</span>.</h5>`
+    );
+  }, 6250);
   setTimeout(function () {
-    $("#home").html(`<br><br><h5 id='home-text5'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>THING</span>.</h5><br>
-    <img className='home-image' src=${require('../../assets/images/grayLogo.png')} />`);
-  }, 8000)
-
+    $(
+      "#home"
+    ).html(`<br><br><h5 id='home-text5'>DISCOVER YOUR NEXT FAVORITE <span class ='main'>THING</span>.</h5><br>
+    <img className='home-image' src=${require("../../assets/images/grayLogo.png")} />`);
+  }, 8000);
 
   return (
     <Container>
@@ -116,31 +133,47 @@ const Home = (props) => {
         <Col s={3}>
           <div className="suggestions">
             <h6 className="title center-align">SUGGESTIONS</h6>
-            <img src="https://www.emmys.com/sites/default/files/styles/marquee_main/public/marquees/cta-2020-40th-900x600.jpg?itok=PYFZdWI0"
+            <img
+              src="https://www.emmys.com/sites/default/files/styles/marquee_main/public/marquees/cta-2020-40th-900x600.jpg?itok=PYFZdWI0"
               className="home-image"
             />
             <div className="default-text">
-              <p><strong>Date: </strong>September 20, 2020</p>
-              <p><strong>Time: </strong>8:00 PM EDT</p>
-              <p><strong>Network: </strong>ABC</p>
-              <p> <strong>Presented by: </strong>Academy of Television Arts & Sciences</p>
+              <p>
+                <strong>Date: </strong>September 20, 2020
+              </p>
+              <p>
+                <strong>Time: </strong>8:00 PM EDT
+              </p>
+              <p>
+                <strong>Network: </strong>ABC
+              </p>
+              <p>
+                {" "}
+                <strong>Presented by: </strong>Academy of Television Arts &
+                Sciences
+              </p>
             </div>
           </div>
         </Col>
         <Col s={6}>
           <div className="middle-content">
-            <div id='home' className='center-align'>
-              <img src={require('../../assets/images/countdown.gif')}
-                id='home-image'
+            <div id="home" className="center-align">
+              <img
+                src={require("../../assets/images/countdown.gif")}
+                id="home-image"
               />
             </div>
 
             <br></br>
-            <h5 className="middle-title">DISCOVER YOUR NEXT FAVORITE MOVIE OR TV SHOW</h5>
+            <h5 className="middle-title">
+              DISCOVER YOUR NEXT FAVORITE MOVIE OR TV SHOW
+            </h5>
             <p className="main-paragraph">
-              entertainME allows you to search for the latest in trending Movies and TV Shows. Or, looking for a classic?
-              You can find that too! Browse for any film or film related celebrity, add to your favorites and share with
-              your friends. Your one stop shop for all things entertainment.
+              entertainME allows you to search for the latest in trending Movies
+              and TV Shows. Or, looking for a classic? You can find that too!
+              Browse for any film or film related celebrity, add to your
+              favorites and share with your friends. Your one stop shop for all
+              things entertainment.
             </p>
           </div>
         </Col>
@@ -163,10 +196,9 @@ const Home = (props) => {
               <li>Katy Perry</li>
             </ul>
             <br></br>
-            <hr className='divider'></hr>
+            <hr className="divider"></hr>
             <br></br>
-            {token ? (<Chat />) : (console.log('User not logged in'))}
-
+            {/* {token ? (<Chat />) : (console.log('User not logged in'))} */}
           </div>
         </Col>
       </Row>
