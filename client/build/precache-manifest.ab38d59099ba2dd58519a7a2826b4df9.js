152f597db29f3c95bbbba0f8aae0d4fcde4d42a0
Admin <janviermbilizi@gmail.com>
2020-05-31 20:31:11 -0600

    Made build folder for client side for production

---
+++ client/build/precache-manifest.ab38d59099ba2dd58519a7a2826b4df9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2544bcfc96ff5261f9bb",
+    "url": "/static/css/main.a39ec1e6.chunk.css"
+  },
+  {
+    "revision": "2544bcfc96ff5261f9bb",
+    "url": "/static/js/main.e0776f07.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fe96c79b51a1306ca374",
+    "url": "/static/js/2.4638e65d.chunk.js"
+  },
+  {
+    "revision": "2608d598cd745dcccb6a9dd12c53d32c",
+    "url": "/static/media/logo.2608d598.png"
+  },
+  {
+    "revision": "44b7e5e46851cbe399a5be7f5f91da66",
+    "url": "/static/media/countdown.44b7e5e4.gif"
+  },
+  {
+    "revision": "9c3f413fadff18e9b0249742bdac984a",
+    "url": "/static/media/scoob.9c3f413f.jpg"
+  },
+  {
+    "revision": "4273b201485d71a7bf07605723361585",
+    "url": "/static/media/dwayne-johnson.4273b201.jpg"
+  },
+  {
+    "revision": "5a5b9f6aacecbde2d9aac83ea4536505",
+    "url": "/static/media/Vin-Diesel.5a5b9f6a.jpg"
+  },
+  {
+    "revision": "91f9df15136dfa69f3705b5b6ad24338",
+    "url": "/index.html"
+  }
+];
