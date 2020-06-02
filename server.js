9a82a164ad3d8a3c9d9880abc0386776257407b9
Admin <janviermbilizi@gmail.com>
2020-06-01 20:55:23 -0600

    added some code to try fixing heroku router error

--- server.js
+++ server.js
@@ -42,8 +42,16 @@ app.get("/test", (req, res) => {
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, "./client/build", "index.html");
+  if (!url.startsWith("/app/"))
+    // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 
 // Mongodb connection
